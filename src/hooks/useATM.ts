b0374492059be91b171ac77fdab4f77c1eb1827f
ecaballero-dev <caballero.edv@gmail.com>
2025-01-18 16:00:48 -0600

    Updated types on useATM and types definition file

--- src/hooks/useATM.ts
+++ src/hooks/useATM.ts
@@ -1,17 +1,18 @@
+import { UserInfo } from '../types'
 const useATM = () => {
-  const getPinInfo = (itemKey: string) => JSON.parse(localStorage.getItem(itemKey) as any)
+  const getPinInfo = (itemKey: string) => JSON.parse(localStorage.getItem(itemKey) as string)
 
-  const handleOperation = (itemKey: string, itemValue: any) => {
-    if (localStorage.getItem(itemKey) && itemValue !== JSON.parse(localStorage.getItem(itemKey) as any).balance) {
+  const handleOperation = (itemKey: string, itemValue: number) => {
+    if (localStorage.getItem(itemKey) && itemValue !== JSON.parse(localStorage.getItem(itemKey) as string).balance) {
       localStorage.setItem(itemKey, JSON.stringify({
-        name: JSON.parse(localStorage.getItem(itemKey) as any).name,
-        card: JSON.parse(localStorage.getItem(itemKey) as any).card,
+        name: JSON.parse(localStorage.getItem(itemKey) as string).name,
+        card: JSON.parse(localStorage.getItem(itemKey) as string).card,
         balance: itemValue,
       }))
     }
   }
 
-  const initializePin = (itemKey: string, itemValue: any) => {
+  const initializePin = (itemKey: string, itemValue: UserInfo) => {
     // Verify pin hasn't been used before
     if (!localStorage.getItem(itemKey)) localStorage.setItem(itemKey, JSON.stringify(itemValue))
   }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,7 @@
 export type Step = 'DEPOSIT' | 'WITHDRAW' | 'BALANCE' | 'ATM' | 'INITIAL' | 'PINPAD';
+
+export type UserInfo = {
+  name: string;
+  balance: number;
+  card: string;
+}
